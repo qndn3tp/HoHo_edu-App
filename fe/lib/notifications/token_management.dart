04d2e2c76bf32f214789456dd53241013046485f
qndn3tp <qndn3tp@naver.com>
2024-04-18 14:05:40 +0900

    Feat: add iOS getToken

--- fe/lib/notifications/token_management.dart
+++ fe/lib/notifications/token_management.dart
@@ -1,9 +1,20 @@
 // 디바이스 토큰을 가져옴
+import 'dart:io';
+
 import 'package:firebase_messaging/firebase_messaging.dart';
 // import 'package:http/http.dart' as http;
 
 void getMyDeviceToken() async {
-  final token = await FirebaseMessaging.instance.getToken();
+  FirebaseMessaging firebaseMessaging = FirebaseMessaging.instance;
+
+  String? token;
+
+  // 토큰
+  if (Platform.isIOS) {
+    token = await firebaseMessaging.getAPNSToken();
+  } else if (Platform.isAndroid) {
+    token = await firebaseMessaging.getToken();
+  }
   print("내 디바이스 토큰: $token");
   // sendToken(token);
 }
